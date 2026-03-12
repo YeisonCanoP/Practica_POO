ec85d7b92efeff199cbfff640febd79fc72c94c6
YeisonCano <yeisonc2002@gmail.com>
2026-03-12 10:49:17 -0500

    Se quitaron comas que estorbaban

--- src/Utils/Operation.java
+++ src/Utils/Operation.java
@@ -5,20 +5,20 @@ public class Operation {
     //Metodo para calcular la suma de dos numeros
     public static double calcular_suma (double a, double b) {
       return a + b;
-    };
+    }
 
     //Metodo para calculcar el salaio bruto seugno precio hora y las horas trabajadas
     public static double calcular_sueldo_bruto (int hours, double precio_hora) {
         return hours * precio_hora;
-    };
+    }
 
     //Metodo para calcular la retencion de un sueldo bruto
     public static double calcular_retencion (double bruto, double retencion) {
         return bruto * retencion;
-    };
+    }
 
     //Metodo para calcucar el salario neto de un empleado
     public static double calcular_sueldo_neto (double bruto, double retencion) {
         return bruto - retencion;
-    };
+    }
 }
